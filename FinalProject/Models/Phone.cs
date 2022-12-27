5035aa47f07fd8d0009fa0d2791f6a3acdbafa6f
Lâm Minh Tuấn <tuanlam300@gmail.com>
2022-12-27 14:09:45 +0700

    done sth

---
+++ FinalProject/Models/Phone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Models
+{
+    [Table("PHONES")]
+    public partial class Phone
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+        [StringLength(250)]
+        public string Ten { get; set; } = null!;
+        [StringLength(250)]
+        public string Hang { get; set; } = null!;
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Gia { get; set; }
+        [StringLength(250)]
+        public string ManHinh { get; set; } = null!;
+        [Column("OS")]
+        [StringLength(250)]
+        public string Os { get; set; } = null!;
+        [StringLength(250)]
+        public string CameraSau { get; set; } = null!;
+        [StringLength(250)]
+        public string CameraTruoc { get; set; } = null!;
+        [StringLength(250)]
+        public string Chip { get; set; } = null!;
+        public int Ram { get; set; }
+        public int Rom { get; set; }
+        [StringLength(250)]
+        public string Pin { get; set; } = null!;
+        [StringLength(550)]
+        public string ImagePath { get; set; } = null!;
+    }
+}
--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,8 +1,10 @@
+using FinalProject.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ASPContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("constring")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
