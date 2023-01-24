0fe765abec9128abcc969dbb1b6b09abfdbb1526
Lâm Minh Tuấn <tuanlam300@gmail.com>
2023-01-24 23:39:22 +0700

    add items in cart count

---
+++ FinalProject/Models/CtgiohangsDAL.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalProject.Models
+{
+    public class CtgiohangsDAL
+    {
+        private static ASPContext _context;
+
+        public static int GetSLInCart()
+        {
+
+            ASPContext context = new ASPContext();
+            _context = context;
+            int count = _context.Giohangs.Count() - 1;
+            return _context.Ctgiohangs.Where(b => b.Idgh == count).Count();
+            
+        }
+    }
+}
