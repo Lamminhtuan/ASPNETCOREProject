19b1e799e244c9c4b7346850730e14ca9411ca44
Lamminhtuan <tuanlam300@gmail.com>
2023-04-29 22:22:21 +0700

    Done

---
+++ FinalProject/Models/PhonesDAL.cs
@@ -0,0 +1,25 @@
+﻿namespace FinalProject.Models
+{
+    public class PhonesDAL
+    {
+        private static ASPContext _context;
+        public static void KhoiTao()
+        {
+            _context = new ASPContext();
+        }
+        public static string GetID()
+        {
+            var kq = _context.Phones.SingleOrDefault(b => b.Id.Substring(2) == _context.Phones.Max(x => x.Id.Substring(2)));
+            int id = Convert.ToUInt16(kq.Id.Substring(2));
+            int id1 = id + 1;
+            if (id < 10)
+            {
+                return "DT0" + id1.ToString();
+            }
+            else
+            {
+                return "DT" + id1.ToString();
+            }
+        }
+    }
+}
---
+++ FinalProject/Models/Reviewlaptop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalProject.Models
+{
+    public partial class Reviewlaptop
+    {
+        public string Idgh { get; set; } = null!;
+        public string Idsp { get; set; } = null!;
+        public int Sao { get; set; }
+        public string BinhLuan { get; set; } = null!;
+
+        public virtual Giohang IdghNavigation { get; set; } = null!;
+        public virtual Laptop IdspNavigation { get; set; } = null!;
+    }
+}
